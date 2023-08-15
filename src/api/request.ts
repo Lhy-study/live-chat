5d1ccb47dfa4648c20723eecafb45065ddc3f4ad
LiuHouYu <Lhy17329785877@outlook.com>
2023-08-15 23:44:10 +0800

    完善了axios的二次封装（增加了缓存数据并设置了过期时间）

--- src/api/request.ts
+++ src/api/request.ts
@@ -1,4 +1,5 @@
 import request from "../axios";
+import { LRUCache } from "lru-cache"
 
 interface configType{
     url:string
@@ -12,16 +13,27 @@ const initRequest=(config:configType)=>{
     return request({...config})
 }
 
-//已经完成还在请求中禁止重复发起请求
+//已经完成还在请求中禁止重复发起请求 缓存数据
 const myRequest=(()=>{
     let urlArr:Array<string>=[];
+    const cache=new LRUCache({
+        maxSize:100,//最大有效数
+        ttl:3 * 60 * 1000//有效时间
+    })
+
     return (config:configType)=>{
         const {url}=config;
+
+        if(cache.get(url)){//代表有已经缓存了
+            return Promise.resolve(cache.get(url))
+        }
+
         if(urlArr.includes(url)){
             return Promise.reject("已经在请求了，请勿频繁发送")
         }
         urlArr.push(url);
-        request({...config}).then(()=>{
+        request({...config}).then((res)=>{
+            cache.set(url,res)
             urlArr=urlArr.filter((item)=>{
                 return item!=url
             })
